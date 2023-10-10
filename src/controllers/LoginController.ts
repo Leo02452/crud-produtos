6de67070e111777d0bd7f600214b15432fb11fcc
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-10 20:33:03 -0300

    feat: add login controller and factory

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,17 @@
+import { ILoginDTO } from '../providers/implementations/schemas/Login';
+import LoginService from '../services/LoginService';
+import { IHttpRequest, IHttpResponse } from './protocols/IHttp';
+import { IRequestValidator } from './protocols/IRequestValidator';
+
+export default class LoginController {
+  constructor(
+    private _loginService: LoginService,
+    private _bodyValidator: IRequestValidator,
+  ) { }
+
+  async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const dto = await this._bodyValidator.parseAsync(httpRequest.body) as ILoginDTO;
+    const token = await this._loginService.execute(dto);
+    return { status: 200, body: { token } };
+  }
+}
---
+++ src/factories/implementations/LoginControllerFactory.ts
@@ -0,0 +1,28 @@
+import LoginController from '../../controllers/LoginController';
+import prismaModel from '../../database/prisma';
+import PasswordProviderAdapter from '../../providers/implementations/PasswordProviderAdapter';
+import TokenProviderAdapter from '../../providers/implementations/TokenProviderAdapter';
+import { loginSchema } from '../../providers/implementations/schemas/Login';
+import UserRepository from '../../repositories/implementations/UserRepository';
+import LoginService from '../../services/LoginService';
+
+export default class LoginControllerFactory {
+  public static make() {
+    const userRepository = new UserRepository(prismaModel.user);
+    const passwordProvider = new PasswordProviderAdapter();
+    const tokenProvider = new TokenProviderAdapter();
+
+    const loginService = new LoginService(
+      userRepository,
+      passwordProvider,
+      tokenProvider,
+    );
+
+    const loginController = new LoginController(
+      loginService,
+      loginSchema,
+    );
+
+    return loginController;
+  }
+}
