4200887a9221af20278ec0406fcc51d6510d3f36
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-11 12:34:55 -0300

    feat: implements list all products route

--- src/routes/ProductRoutes.ts
+++ src/routes/ProductRoutes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import CreateProductControllerFactory
   from '../factories/implementations/CreateProductControllerFactory';
+import ListProductsControllerFactory
+  from '../factories/implementations/ListProductsControllerFactory';
 
 const createProductController = CreateProductControllerFactory.make();
+const listProductsController = ListProductsControllerFactory.make();
 
 const route = Router();
+
+route.get(
+  '/',
+  async (req, res) => {
+    const response = await listProductsController.handle();
+    return res.status(response.status).json(response.body);
+  },
+);
 
 route.use(authValidation);
 
