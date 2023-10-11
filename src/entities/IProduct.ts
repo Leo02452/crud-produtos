800a92abf03b29630f4cb1809c6d3ac0a8889bed
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-11 13:05:30 -0300

    chore: delete remaining src folder

--- src/entities/IProduct.ts
+++
@@ -1,5 +0,0 @@
-import { ICreateProductDTO } from '../providers/implementations/schemas/Product';
-
-export interface IProduct extends ICreateProductDTO {
-  id: number
-}
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import userRoutes from './UserRoutes';
-import authRoutes from './AuthRoutes';
-import productRoutes from './ProductRoutes';
-
-export {
-  userRoutes,
-  authRoutes,
-  productRoutes,
-};
