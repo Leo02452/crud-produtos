1963560ee6cc0530400336df66f195ed7ed5ad8b
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-11 12:33:02 -0300

    feat: add list all products repository
    add interface and implementation

--- src/repositories/IProductRepository.ts
+++ src/repositories/IProductRepository.ts
@@ -1,7 +1,12 @@
+import { IProduct } from '../entities/IProduct';
 import {
   ICreateProductDTO,
 } from '../providers/implementations/schemas/Product';
 
 export interface ICreateProductRepository {
   save(data: ICreateProductDTO): Promise<void>
+}
+
+export interface IFindAllProductsRepository {
+  getAll(): Promise<IProduct[]>
 }
--- src/repositories/implementations/ProductRepository.ts
+++ src/repositories/implementations/ProductRepository.ts
@@ -1,18 +1,27 @@
 import prismaModel from '../../database/prisma';
+import { IProduct } from '../../entities/IProduct';
 import {
   ICreateProductDTO,
 } from '../../providers/implementations/schemas/Product';
 import {
   ICreateProductRepository,
+  IFindAllProductsRepository,
 } from '../IProductRepository';
 
 export default class ProductRepository implements
-ICreateProductRepository {
+ICreateProductRepository,
+IFindAllProductsRepository {
   constructor(
     private _model: typeof prismaModel.product,
   ) { }
 
   async save(data: ICreateProductDTO): Promise<void> {
     await this._model.create({ data });
+  }
+
+  async getAll(): Promise<IProduct[]> {
+    const productsList = await this._model.findMany();
+
+    return productsList;
   }
 }
