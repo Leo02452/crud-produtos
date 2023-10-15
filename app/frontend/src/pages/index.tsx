929d50cfdd5d3c31ec3bc8157a0d19bd08e8d45d
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-15 17:33:28 -0300

    feat: implements login route

--- app/frontend/src/pages/index.tsx
+++ app/frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Registration from './registration';
+import Login from './login';
 
 export default function ApplicationRoutes() {
   return (
     <Routes>
       <Route path="/registration" element={<Registration />} />
+      <Route path="/signin" element={<Login />} />
     </Routes>
   );
 }
