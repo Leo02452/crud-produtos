63392fe5a6854448540824ca985117192e4eb6a6
Leo Araujo <leonardo_02452@hotmail.com>
2023-10-15 18:01:12 -0300

    feat: implements create product feature

--- app/frontend/src/pages/products.tsx
+++ app/frontend/src/pages/products.tsx
@@ -9,11 +9,13 @@ import {
 import { useCallback, useState } from 'react';
 import { IProduct } from '../application/dto-and-entities/product';
 import ProductCard from '../components/cards/product.card';
+import CreateProductModal from '../components/modals/create-product.modal';
 import useAuth from '../hooks/contexts/auth.context-hook';
 import useGetAllProducts from '../hooks/queries/get-products.query-hook';
 
 export default function Products() {
   const { isAuthenticated } = useAuth();
+  const createProductModalControl = useDisclosure();
 
   const { data } = useGetAllProducts();
 
@@ -25,6 +27,16 @@ export default function Products() {
       alignItems="center"
       backgroundColor="main.500"
     >
+      <CreateProductModal
+        isOpen={createProductModalControl.isOpen}
+        onClose={createProductModalControl.onClose}
+      />
+        {isAuthenticated && (
+          <Button onClick={createProductModalControl.onOpen} color="secundary">
+            Criar novo produto
+          </Button>
+        )
+        }
       <Stack width="90%" flexDirection="row" flexGrow={1} spacing={8} p={4}>
         {data?.map((product: IProduct) => (
           <ProductCard
